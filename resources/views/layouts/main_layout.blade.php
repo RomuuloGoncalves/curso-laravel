c895f0c355b6ebed8f9d26d0cd8e2296d7e02904
RomuuloGoncalves <romulogoncalves13@gmail.com>
2024-09-28 17:46:39 -0300

    teste laravel

---
+++ resources/views/layouts/main_layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Main</title>
+</head>
+<body>
+    <p>Text from the layout (TOP)</p>
+    <hr>
+    @yield('content')
+    <hr>
+    <p>Text from the layout (BOTTOM)</p>
+</body>
+</html> 
